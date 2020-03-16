7febe20f5e5614897e9e5e87dad84889e6f313d6
Pavel <ponomarenko.pavel.web@gmail.com>
2020-03-16 12:29:07 +0200

    fixed by Egor

--- single-sn_catalogs.php
+++ single-sn_catalogs.php
@@ -9,20 +9,22 @@
 <div class="row">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="col-lg-5">
-			<?php the_post_thumbnail('medium'); ?>
+			<?php the_post_thumbnail('large'); ?>
+			<div class="sp-xs-3 sp-sm-3 sp-md-1"></div>
 		</div>
 		<div class="col-lg-7">
 			<h1 class="single-title"><?php the_title() ?></h1>
 			<div class="sp-xs-2"></div>
 			<div class="prices"><?php the_excerpt(); ?></div>
 			<div class="sp-xs-2"></div>
-			<div><?php the_content(); ?></div>
-			<div class="sp-xs-2"></div>
 			<button class="btn btn-secondary <?php the_lang_class('one-click'); ?>">Заказать</button>
 		</div>
+        <div class="col-12">
+			<div><?php the_content(); ?></div>
+        </div>
 	<?php endwhile; ?>
 <?php endif; ?>
 </div>
 </div><!-- /.container -->
-
+<div class="sp-xs-3"></div>
 <?php get_footer(); ?>
